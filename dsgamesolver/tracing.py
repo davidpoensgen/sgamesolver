7538e12f41fbef64f1f85ce3062e1ea83385a60c
steffen <steffen.eibelshaeuser@gmail.com>
2021-06-03 02:21:24 +0200

    angle warning corrected

--- dsgamesolver/tracing.py
+++ dsgamesolver/tracing.py
@@ -2,7 +2,6 @@
 
 # TODO: check user-provided priors and weights?
 # TODO: maybe write custom optimization for find_y0 to avoid scipy import
-# TODO: starting point seems to be (almost) tangent to t=0, is that correct?
 
 # TODO: play with einsum_path
 # TODO: adjust tracking parameters with "scale" of game
