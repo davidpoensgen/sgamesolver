dc3c92c6ac6f290aff2f9aea7cd3b275ce4dfe26
David Poensgen <davidpoensgen@gmail.com>
2022-04-02 17:27:48 +0200

    Separated declarations and assignments in H, J.

--- sgamesolver/homotopy/_qre_ct.pyx
+++ sgamesolver/homotopy/_qre_ct.pyx
@@ -8,6 +8,217 @@ cimport numpy as np
 np.import_array()
 
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def H(np.ndarray[np.float64_t, ndim=1] y, u, phi, int num_s, int num_p,
+      np.ndarray[np.int32_t, ndim=2] nums_a, int num_a_max, int num_a_tot):
+    """Homotopy function.
+
+    H(y) = [  H_strat[s,i,a]  ]
+           [  H_val[s,i]      ]
+    with
+    y = [ beta[s,i,a],  V[s,i],  lambda ]
+    """
+
+    cdef:
+        np.ndarray[np.float64_t, ndim=1] out_ = np.zeros(num_a_tot + num_s*num_p)
+        np.ndarray[np.float64_t, ndim=3] beta = np.zeros((num_s, num_p, num_a_max))
+        np.ndarray[np.float64_t, ndim=3] sigma
+        np.ndarray[np.float64_t, ndim=2] V = y[num_a_tot : num_a_tot + num_s*num_p].reshape(num_s, num_p)
+        double lambda_ = y[num_a_tot + num_s*num_p]
+
+        np.ndarray[np.float64_t, ndim=1] u_tilde_ev_ravel
+        np.ndarray[np.float64_t, ndim=3] u_tilde_sia_ev
+
+        int state, player, action, a
+        int flat_index = 0
+
+    for state in range(num_s):
+        for player in range(num_p):
+            for action in range(nums_a[state, player]):
+                beta[state, player, action] = y[flat_index]
+                flat_index += 1
+
+    sigma = np.exp(beta)
+    u_tilde_ev_ravel = u_tilde(u, V, phi).ravel()
+    u_tilde_sia_ev = u_tilde_sia(u_tilde_ev_ravel, sigma, num_s, num_p, nums_a, num_a_max)
+
+    flat_index = 0
+    for state in range(num_s):
+        for player in range(num_p):
+            for action in range(nums_a[state, player]):
+
+                if action == 0:
+                    out_[flat_index] += 1
+                    for a in range(nums_a[state, player]):
+                        out_[flat_index] -= sigma[state, player, a]
+                else:
+                    out_[flat_index] = (beta[state, player, 0] - beta[state, player, action] + lambda_ *
+                                        (u_tilde_sia_ev[state,player,action] - u_tilde_sia_ev[state,player,0]))
+
+                flat_index += 1
+
+    for state in range(num_s):
+        for player in range(num_p):
+            out_[flat_index] -= V[state, player]
+            for action in range(nums_a[state, player]):
+                out_[flat_index] += sigma[state, player, action] * u_tilde_sia_ev[state, player, action]
+            flat_index += 1
+
+    return out_
+
+
+# %% Jacobian matrix
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def J(np.ndarray[np.float64_t, ndim=1] y, u, phi, int num_s, int num_p,
+      np.ndarray[np.int32_t, ndim=2] nums_a, int num_a_max, int num_a_tot):
+    """Jacobian matrix.
+
+    J(y) = [  d_H_strat[s,i,a] / d_beta[s',i',a'],  d_H_strat[s,i,a] / d_V[s',i'],  d_H_strat[s,i,a] / d_lambda  ]
+           [  d_H_val[s,i]     / d_beta[s',i',a'],  d_H_val[s,i]     / d_V[s',i'],  d_H_val[s,i]     / d_lambda  ]
+    with
+    y = [ beta[s,i,a],  V[s,i],  lambda ]
+    """
+
+    cdef:
+        np.ndarray[np.float64_t, ndim=2] out_ = np.zeros((num_a_tot + num_s*num_p, num_a_tot + num_s*num_p + 1))
+        np.ndarray[np.float64_t, ndim=3] beta = np.zeros((num_s, num_p, num_a_max))
+        np.ndarray[np.float64_t, ndim=3] sigma
+        np.ndarray[np.float64_t, ndim=2] V = y[num_a_tot : num_a_tot + num_s*num_p].reshape(num_s, num_p)
+        double lambda_ = y[num_a_tot + num_s*num_p]
+
+        np.ndarray[np.float64_t, ndim=1] u_tilde_ev_ravel
+        np.ndarray[np.float64_t, ndim=3] u_tilde_sia_ev
+        np.ndarray[np.float64_t, ndim=5] u_tilde_sia_partial_beta_ev
+        np.ndarray[np.float64_t, ndim=5] u_tilde_sia_partial_V_ev
+
+        int state, player, action
+        int flat_index = 0
+        int row_state, row_player, row_action
+        int col_state, col_player, col_action
+        int row_index, col_index, col_index_init
+
+    for state in range(num_s):
+        for player in range(num_p):
+            for action in range(nums_a[state, player]):
+                beta[state, player, action] = y[flat_index]
+                flat_index += 1
+
+    sigma = np.exp(beta)
+    u_tilde_ev_ravel = u_tilde(u, V, phi).ravel()
+    u_tilde_sia_ev = u_tilde_sia(u_tilde_ev_ravel, sigma, num_s, num_p, nums_a, num_a_max)
+    u_tilde_sia_partial_beta_ev = (u_tilde_sia_partial_beta(u_tilde_ev_ravel, sigma, num_s, num_p, nums_a, num_a_max))
+    u_tilde_sia_partial_V_ev = (u_tilde_sia_partial_V(phi.ravel(), sigma, num_s, num_p, nums_a, num_a_max))
+
+    # first block: rows with d_H_strat[s,i,a]
+    row_index = 0
+    col_index_init = 0
+    for row_state in range(num_s):
+        for row_player in range(num_p):
+            for row_action in range(nums_a[row_state, row_player]):
+
+                # derivatives w.r.t. beta[s',i',a']
+                # entries with s' != s are 0, thus no looping over s'
+                col_index = col_index_init
+                for col_player in range(num_p):
+                    for col_action in range(nums_a[row_state, col_player]):
+
+                        # diagonal sub-blocks: derivatives w.r.t. beta[s,i,a']
+                        # (own actions in same state)
+                        if row_player == col_player:
+                            if row_action == 0:
+                                out_[row_index, col_index] = -sigma[row_state, col_player, col_action]
+                            else:
+                                if col_action == 0:
+                                    out_[row_index, col_index] = 1
+                                elif row_action == col_action:
+                                    out_[row_index, col_index] = -1
+
+                        # off-diagonal sub-blocks: derivatives w.r.t. beta[s,i',a']
+                        # (other players' actions in same state)
+                        else:
+                            # row_action == 0 -> entry = 0
+                            if row_action != 0:
+                                out_[row_index, col_index] = lambda_ * (
+                                      u_tilde_sia_partial_beta_ev[row_state, row_player, row_action, col_player, col_action]
+                                    - u_tilde_sia_partial_beta_ev[row_state, row_player,          0, col_player, col_action]
+                                    )
+
+                        col_index += 1
+
+                # derivatives w.r.t. V[s',i']
+                col_index = num_a_tot
+                for col_state in range(num_s):
+                    for col_player in range(num_p):
+                        if row_action != 0:
+                            out_[row_index, col_index] = lambda_ * (
+                                  u_tilde_sia_partial_V_ev[row_state, row_player, row_action, col_state, col_player]
+                                - u_tilde_sia_partial_V_ev[row_state, row_player,          0, col_state, col_player]
+                                )
+                        col_index += 1
+
+                # derivative w.r.t. lambda
+                # row_action == 0 -> entry = 0
+                if row_action != 0:
+                    out_[row_index, col_index] = (u_tilde_sia_ev[row_state, row_player, row_action]
+                                                  - u_tilde_sia_ev[row_state, row_player, 0])
+
+                row_index += 1
+
+        for row_player in range(num_p):
+            col_index_init += nums_a[row_state,row_player]
+
+    # second block: rows with d_H_val[s,i]
+    row_index = num_a_tot
+    col_index_init = 0
+    for row_state in range(num_s):
+        for row_player in range(num_p):
+
+            # derivatives w.r.t. beta[s',i',a']
+            # entries with s' != s are 0, thus no looping over s'
+            col_index = col_index_init
+
+            for col_player in range(num_p):
+                for col_action in range(nums_a[row_state, col_player]):
+
+                    if row_player == col_player:
+                        out_[row_index, col_index] += (sigma[row_state, row_player, col_action]
+                                                       * u_tilde_sia_ev[row_state, row_player, col_action])
+                    else:
+                        for row_action in range(nums_a[row_state, row_player]):
+                            out_[row_index, col_index] += (sigma[row_state, row_player, row_action]
+                                * u_tilde_sia_partial_beta_ev[row_state, row_player, row_action, col_player, col_action]
+                                )
+
+                    col_index += 1
+
+            # derivatives w.r.t. V[s',i']
+            col_index = num_a_tot
+            for col_state in range(num_s):
+                for col_player in range(num_p):
+
+                    if col_state == row_state and col_player == row_player:
+                        out_[row_index, col_index] -=1
+                    for row_action in range(nums_a[row_state, row_player]):
+                        out_[row_index, col_index] += (sigma[row_state, row_player, row_action]
+                            * u_tilde_sia_partial_V_ev[row_state, row_player, row_action, col_state, col_player]
+                            )
+
+                    col_index += 1
+
+            # derivative w.r.t. lambda = 0
+
+            row_index += 1
+
+        for row_player in range(num_p):
+            col_index_init += nums_a[row_state, row_player]
+
+    return out_
+
+
 # %% auxiliary functions
 
 
@@ -148,206 +359,32 @@ def u_tilde_sia_partial_V(np.ndarray[np.float64_t, ndim=1] phi_ravel, np.ndarray
 # %% homotopy function
 
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def H(np.ndarray[np.float64_t, ndim=1] y, u, phi, int num_s, int num_p,
-      np.ndarray[np.int32_t, ndim=2] nums_a, int num_a_max, int num_a_tot):
-    """Homotopy function.
-
-    H(y) = [  H_strat[s,i,a]  ]
-           [  H_val[s,i]      ]
-    with
-    y = [ beta[s,i,a],  V[s,i],  lambda ]
-    """
-
-    cdef:
-        np.ndarray[np.float64_t, ndim=1] out_ = np.zeros(num_a_tot + num_s*num_p)
-        np.ndarray[np.float64_t, ndim=3] beta = np.zeros((num_s, num_p, num_a_max))
-        int state, player, action, a
-        int flat_index = 0
-
-    for state in range(num_s):
-        for player in range(num_p):
-            for action in range(nums_a[state, player]):
-                beta[state, player, action] = y[flat_index]
-                flat_index += 1
-
-    cdef:
-        np.ndarray[np.float64_t, ndim=3] sigma = np.exp(beta)
-        np.ndarray[np.float64_t, ndim=2] V = y[num_a_tot : num_a_tot + num_s*num_p].reshape(num_s, num_p)
-        double lambda_ = y[num_a_tot + num_s*num_p]
-        np.ndarray[np.float64_t, ndim=1] u_tilde_ev_ravel = u_tilde(u, V, phi).ravel()
-        np.ndarray[np.float64_t, ndim=3] u_tilde_sia_ev = u_tilde_sia(u_tilde_ev_ravel, sigma, num_s, num_p,
-                                                                      nums_a, num_a_max)
-
-    flat_index = 0
-    for state in range(num_s):
-        for player in range(num_p):
-            for action in range(nums_a[state, player]):
-
-                if action == 0:
-                    out_[flat_index] += 1
-                    for a in range(nums_a[state, player]):
-                        out_[flat_index] -= sigma[state, player, a]
-                else:
-                    out_[flat_index] = (beta[state, player, 0] - beta[state, player, action] + lambda_ *
-                                        (u_tilde_sia_ev[state,player,action] - u_tilde_sia_ev[state,player,0]))
-
-                flat_index += 1
-
-    for state in range(num_s):
-        for player in range(num_p):
-            out_[flat_index] -= V[state, player]
-            for action in range(nums_a[state, player]):
-                out_[flat_index] += sigma[state, player, action] * u_tilde_sia_ev[state, player, action] 
-            flat_index += 1
-
-    return out_
-
-
-# %% Jacobian matrix
-
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def J(np.ndarray[np.float64_t, ndim=1] y, u, phi, int num_s, int num_p, 
-      np.ndarray[np.int32_t, ndim=2] nums_a, int num_a_max, int num_a_tot):
-    """Jacobian matrix.
+cdef bint arrays_equal(double [:] a, double [:] b):
+    """Check if two 1d-arrays are identical."""
+    cdef int i
+    if a.shape[0] != b.shape[0]:
+        return False
+    for i in range(a.shape[0]):
+        if a[i] != b[i]:
+            return False
+    return True
 
-    J(y) = [  d_H_strat[s,i,a] / d_beta[s',i',a'],  d_H_strat[s,i,a] / d_V[s',i'],  d_H_strat[s,i,a] / d_lambda  ]
-           [  d_H_val[s,i]     / d_beta[s',i',a'],  d_H_val[s,i]     / d_V[s',i'],  d_H_val[s,i]     / d_lambda  ]
-    with
-    y = [ beta[s,i,a],  V[s,i],  lambda ]
-    """
 
+cdef class QreCache:
+    """Caches intermediate results during computation of H or J, to be used later by the other Function."""
     cdef:
-        np.ndarray[np.float64_t, ndim=2] out_ = np.zeros((num_a_tot + num_s*num_p, num_a_tot + num_s*num_p + 1))
-        np.ndarray[np.float64_t, ndim=3] beta = np.zeros((num_s, num_p, num_a_max))
-        int state, player, action
-        int flat_index = 0
+        double [::1] y
+        double [:,:,::1] u_sigma
+        double [:,:,:,::1] phi_sigma
+        double [:,:,:,::1] phi_bar
+        double [:,:,::1] u_tilde_sia_ev
 
-    for state in range(num_s):
-        for player in range(num_p):
-            for action in range(nums_a[state, player]):
-                beta[state, player, action] = y[flat_index]
-                flat_index += 1
-
-    cdef:
-        np.ndarray[np.float64_t, ndim=3] sigma = np.exp(beta)
-        np.ndarray[np.float64_t, ndim=2] V = y[num_a_tot : num_a_tot + num_s*num_p].reshape(num_s, num_p)
-        double lambda_ = y[num_a_tot + num_s*num_p]
-        np.ndarray[np.float64_t, ndim=1] u_tilde_ev_ravel = u_tilde(u, V, phi).ravel()
-        np.ndarray[np.float64_t, ndim=3] u_tilde_sia_ev = u_tilde_sia(u_tilde_ev_ravel, sigma, num_s, num_p,
-                                                                      nums_a, num_a_max)
-        np.ndarray[np.float64_t, ndim=5] u_tilde_sia_partial_beta_ev = (u_tilde_sia_partial_beta(u_tilde_ev_ravel,
-                                                                        sigma, num_s, num_p, nums_a, num_a_max))
-        np.ndarray[np.float64_t, ndim=5] u_tilde_sia_partial_V_ev = (u_tilde_sia_partial_V(phi.ravel(),
-                                                                     sigma, num_s, num_p, nums_a, num_a_max))
-        int row_state, row_player, row_action
-        int col_state, col_player, col_action
-        int row_index, col_index, col_index_init
-
-    # first block: rows with d_H_strat[s,i,a]
-    row_index = 0
-    col_index_init = 0
-    for row_state in range(num_s):
-        for row_player in range(num_p):
-            for row_action in range(nums_a[row_state, row_player]):
-
-                # derivatives w.r.t. beta[s',i',a']
-                # entries with s' != s are 0, thus no looping over s'
-                col_index = col_index_init
-                for col_player in range(num_p):
-                    for col_action in range(nums_a[row_state, col_player]):
-
-                        # diagonal sub-blocks: derivatives w.r.t. beta[s,i,a']
-                        # (own actions in same state)
-                        if row_player == col_player:
-                            if row_action == 0:
-                                out_[row_index, col_index] = -sigma[row_state, col_player, col_action]
-                            else:
-                                if col_action == 0:
-                                    out_[row_index, col_index] = 1
-                                elif row_action == col_action:
-                                    out_[row_index, col_index] = -1
-
-                        # off-diagonal sub-blocks: derivatives w.r.t. beta[s,i',a']
-                        # (other players' actions in same state)
-                        else:
-                            # row_action == 0 -> entry = 0
-                            if row_action != 0:
-                                out_[row_index, col_index] = lambda_ * (
-                                      u_tilde_sia_partial_beta_ev[row_state, row_player, row_action, col_player, col_action]
-                                    - u_tilde_sia_partial_beta_ev[row_state, row_player,          0, col_player, col_action]
-                                    )
-
-                        col_index += 1
-
-                # derivatives w.r.t. V[s',i']
-                col_index = num_a_tot
-                for col_state in range(num_s):
-                    for col_player in range(num_p):
-                        if row_action != 0:
-                            out_[row_index, col_index] = lambda_ * (  
-                                  u_tilde_sia_partial_V_ev[row_state, row_player, row_action, col_state, col_player]
-                                - u_tilde_sia_partial_V_ev[row_state, row_player,          0, col_state, col_player]
-                                )
-                        col_index += 1
-
-                # derivative w.r.t. lambda
-                # row_action == 0 -> entry = 0
-                if row_action != 0:
-                    out_[row_index, col_index] = (u_tilde_sia_ev[row_state, row_player, row_action]
-                                                  - u_tilde_sia_ev[row_state, row_player, 0])
-
-                row_index += 1
-
-        for row_player in range(num_p):
-            col_index_init += nums_a[row_state,row_player]
-
-    # second block: rows with d_H_val[s,i]
-    row_index = num_a_tot
-    col_index_init = 0
-    for row_state in range(num_s):
-        for row_player in range(num_p):
-
-            # derivatives w.r.t. beta[s',i',a']
-            # entries with s' != s are 0, thus no looping over s'
-            col_index = col_index_init
-
-            for col_player in range(num_p):
-                for col_action in range(nums_a[row_state, col_player]):
-
-                    if row_player == col_player:
-                        out_[row_index, col_index] += (sigma[row_state, row_player, col_action]
-                                                       * u_tilde_sia_ev[row_state, row_player, col_action])
-                    else:
-                        for row_action in range(nums_a[row_state, row_player]):
-                            out_[row_index, col_index] += (sigma[row_state, row_player, row_action]
-                                * u_tilde_sia_partial_beta_ev[row_state, row_player, row_action, col_player, col_action]
-                                )
-
-                    col_index += 1
-
-            # derivatives w.r.t. V[s',i']
-            col_index = num_a_tot
-            for col_state in range(num_s):
-                for col_player in range(num_p):
-
-                    if col_state == row_state and col_player == row_player:
-                        out_[row_index, col_index] -=1
-                    for row_action in range(nums_a[row_state, row_player]):
-                        out_[row_index, col_index] += (sigma[row_state, row_player, row_action]
-                            * u_tilde_sia_partial_V_ev[row_state, row_player, row_action, col_state, col_player]
-                            )
-
-                    col_index += 1
-
-            # derivative w.r.t. lambda = 0
-
-            row_index += 1
-
-        for row_player in range(num_p):
-            col_index_init += nums_a[row_state, row_player]
-
-    return out_
+    def __cinit__(self):
+        self.y = np.zeros(1)
+        self.u_sigma = np.zeros((1,1,1))
+        self.phi_sigma = np.zeros((1,1,1,1))
+        self.phi_bar = np.zeros((1,1,1,1))
+        self.u_tilde_sia_ev = np.zeros((1,1,1))
